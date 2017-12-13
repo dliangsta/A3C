79798ba75c2124f8dfff4d3f125f98e322797dce
David Liang <davidliangx27@gmail.com>
2017-12-12 18:22:02 -0600

    stuff

--- table/agent.py
+++ table/agent.py
@@ -1,60 +1,71 @@
 import gym
 import numpy as np
 import pickle
-import os
-
-def main():
-    if os.path.isfile('agent.pkl'):
-        agent = pickle.load(open('agent.pkl','rb'))
-        print('loaded')
-    else:
-        agent = Agent()
-    
-    agent.train()
+from tqdm import trange
 
 class Agent():
 
-    def __init__(self):
+    def __init__(self, run_dir, save_dir):
+        self.run_dir = run_dir
+        self.save_dir = save_dir
+
         self.env = gym.make('FrozenLake8x8-v0')
         self.Q_table = np.zeros([self.env.observation_space.n, self.env.action_space.n])
         self.learning_rate = .8
         self.discount_rate = .99
-        self.iteration = 0
+        self.t = 0
+        self.t_max = 1000000
+        self.test_freq = 1000
+        self.test_max = 1000
+        self.test_len = 30
         self.mean_rewards = []
 
+        self.performance_log_filename = save_dir + 'data/performance_log.csv'
+        self.pkl_filename = save_dir + 'data/agent.pkl'
+
     def train(self):
-        for self.iteration in range(self.iteration, 1000000):
+        t = trange(self.t, self.t_max, desc='rl-thesis', ncols=200)
+        for self.t in t:
             # Train agent.
             self.play_episode(True)
-            if self.iteration % 1000 == 0:
-                # Copy table.
-                # Run for 100 episodes.
+
+            if self.t % self.test_freq == 0:
+                # Run for ___ episodes.
                 rewards = []
-                for game in range(1000):
+                for game in range(self.test_max):
                     reward = self.play_episode(False)
                     rewards.append(reward)
 
+                # Statistics.
                 self.mean_rewards.append(np.mean(rewards))
-                if len(self.mean_rewards) >= 30:
-                    print('iteration: %9d, avg: %.3f, recent_avg: %.9f, recent_var: %.9f, learning_rate: %.9f' % (self.iteration, np.mean(rewards), np.mean(self.mean_rewards[-30:]), np.var(self.mean_rewards[-30:]), self.learning_rate))
+                if len(self.mean_rewards) >= self.test_len:
+                    recent_avg = np.mean(self.mean_rewards[-self.test_len:])
+                    recent_var = np.var(self.mean_rewards[-self.test_len:])
                 else:
-                    print('iteration: %9d, avg: %.3f, learning_rate: %.9f' % (self.iteration, np.mean(rewards) ,self.learning_rate))
+                    recent_avg = 0.
+                    recent_var = 0.
+                status_string = 'iteration: %9d, avg: %.3f, recent_avg: %.9f, recent_var: %.9f, learning_rate: %.9f' % (self.t, np.mean(rewards), recent_avg, recent_var, self.learning_rate)
+                open(self.performance_log_filename,'a').write(status_string.replace(':',',') + '\n')
+
+                # Temporary stopping criterion.
                 if np.mean(rewards) >= .99:
                     print('Done!')
 
+                t.set_description(status_string)
                 self.save()
+                print()
 
     def play_episode(self, train):
         # Reset environment and get first new observation
         state = self.env.reset()
         reward = 0
-        done = False
+        
         # The Q_table-Table learning algorithm
         for step in range(1000):
 
             if train:
                 # Choose an action by greedily (with noise) picking from Q table
-                action = np.argmax(self.Q_table[state, :] + np.random.randn(1, self.env.action_space.n) * 1000. / (self.iteration + 1))
+                action = np.argmax(self.Q_table[state, :] + np.random.randn(1, self.env.action_space.n) * 1000. / (self.t + 1))
             else:
                 # Choose best action from Q table
                 action = np.argmax(self.Q_table[state, :])
@@ -74,14 +85,12 @@ class Agent():
 
         # Save.
         if not train:
-            pickle.dump(self, open('agent.pkl','wb'))
+            pickle.dump(self, open(self.pkl_filename,'wb'))
+
 
         self.learning_rate = max(.08, .99999 * self.learning_rate)
         return reward
 
     def save(self):
-        pickle.dump(self, open('agent.pkl','wb'))
+        pickle.dump(self, open(self.pkl_filename,'wb'))
     
-
-if __name__ == '__main__':
-    main()
